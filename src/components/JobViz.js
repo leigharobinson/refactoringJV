edbd9bc5d625702b6e8fd4dd334086acce92d2e4
leigharobinson <lmarobinson24@gmail.com>
2020-12-30 13:42:40 -0600

    structure

---
+++ src/components/JobViz.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ApplicationViews from "./ApplicationViews";
+
+export const JobViz = () => {
+  return (
+    <>
+      <div> Hello World </div>
+      {/* <ApplicationViews /> */}
+    </>
+  );
+};
